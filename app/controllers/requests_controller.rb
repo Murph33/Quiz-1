d18eb67f0df9a129afb434d40c71d644440ccc48
Murphy Stroppa <murph33@gmail.com>
2015-06-08 09:42:08 -0700

    Added a (bare) welcome page.  Added some routing.  Added ability to create requests, tabularized the requests index

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,22 @@
 class RequestsController < ApplicationController
 
-  
+  def index
+    @requests = Request.all
+  end
+
+  def new
+    @request = Request.new
+  end
+
+  def create
+    request_params = params.require(:request).permit(:name, :email, :department, :message)
+    @request = Request.new request_params
+    if @request.save
+      redirect_to requests_path
+    else
+      flash[:alert] = "Failed to create.  Please correct errors"
+      render :new
+    end
+  end
 
 end
